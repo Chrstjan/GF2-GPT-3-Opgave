81a1f629470c09192c18bf41fcd540922544fd42
Christian <ps5christian@hotmail.com>
2023-12-18 20:04:28 +0100

    Fixed the ball images
    For mobile and desktop

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -38,6 +38,7 @@ const createVrGallery = () => {
       const modalImage = galleryImage.cloneNode(true);
 
       modalFigure.appendChild(modalImage);
+      galleryFigure.appendChild(modalFigure);
 
       modalFigure.addEventListener("click", () => {
         modalFigure.remove();
