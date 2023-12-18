91984ad1a5767c72b8b6ea39426fd5b0d4de0a70
Chrstjan <ps5christian@hotmail.com>
2023-12-18 10:14:00 +0100

    Made the most part

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,3 +1,4 @@
+//Hamburger Menu
 const burgerBtn = document.getElementById("burger-menu");
 const burgerMenuContent = document.querySelectorAll(".main-list li");
 
@@ -9,3 +10,35 @@ burgerBtn.addEventListener("click", () => {
         li.classList.toggle("hamburger-content");
     });
 })
+
+const vrGalleryArray = ['Image-03.jpg', 'Image-05.jpg', 'Image-06.jpg', 'Image-07.jpg', 'Image-08.jpg', 'Image-09.jpg'];
+
+const baseUrl = './Assets/images/';
+
+const galleryFigure = document.getElementById("vr-gallery");
+
+const createVrGallery = () => {
+    vrGalleryArray.forEach((img) => {
+        const galleryImage = document.createElement("img");
+        const galleryImageSovs = baseUrl + img;
+        galleryImage.src = galleryImageSovs;
+    
+        galleryFigure.appendChild(galleryImage);
+
+        const openModal = () => {
+            const modalFigure = document.createElement("figure");
+            modalFigure.classList.add("modal-figure");
+            const modalImage = galleryImage.cloneNode(true);
+
+            modalFigure.appendChild(modalImage);
+
+            modalFigure.addEventListener("click", () => {
+                modalFigure.remove();
+            })
+        }
+
+        galleryImage.addEventListener("click", openModal);
+    });
+}
+
+window.addEventListener("load", createVrGallery);
