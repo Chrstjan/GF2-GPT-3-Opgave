36ae88c72f3dd58a14310ecf307736596010d16b
Chrstjan <ps5christian@hotmail.com>
2023-12-19 12:32:21 +0100

    Made the form validation
    Need to style the form

--- Assets/js/sign-up.js
+++ Assets/js/sign-up.js
@@ -55,6 +55,12 @@ const createSignUpForm = () => {
     const lNameLabel = createFormLabel("Last name", "lName");
     const lName = createFormInput("text", "Doe", "lName");
 
+    const usernameLabel = createFormLabel("Username", "username");
+    const username = createFormInput("text", "johnSlayer12");
+
+    const passwordLabel = createFormLabel("Password", "password");
+    const password = createFormInput("text", "***********", "password");
+
     const emailLabel = createFormLabel("Email", "email");
     const email = createFormInput("email", "none@fake.com", "email");
 
@@ -62,7 +68,62 @@ const createSignUpForm = () => {
     const resetBtn = createFormInputBtn("reset", "Reset");
     const submitBtn = createFormInputBtn("submit", "Submit");
 
-    // submitBtn.addEventListener("click", formValidation);
+    const validateFormInput = (input, regEx, errorMessage) => {
+        const trimmedValue = input.value.trim();
+        const isValid = regEx.test(trimmedValue);
+
+        if (isValid) {
+            console.log("nice");
+        }
+        else {
+            console.log("not nice");
+            displayErrorMessage(errorMessage);
+        }
+
+        return isValid;
+    };
+
+    const displayErrorMessage = (message) => {
+        errorTextContainer.textContent = message;
+    }
+
+    const formValidation = (e) => {
+        const nameRegEx = /^[a-zA-Z]{2,17}$/;
+        const usernamRegEx = /^[0-9A-Za-z]{6,16}$/;
+        const passwordRegEx = /^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$/;
+        const emailRegEx = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/;
+
+        const isFNameValid = validateInput(fName, nameRegEx, "First name must be a valid name");
+        const isLNameValid = validateInput(lName, nameRegEx, "Last name must be a valid name");
+        const isUsernameValid = validateInput(username, usernamRegEx, "Username must be a valid username");
+        const isPasswordValid = validateInput(password, passwordRegEx, "Pasword must be a valid password");
+        const isEmailValid = validateInput(email, emailRegEx, "Email must be a valid email");
+
+        if (isFNameValid && isLNameValid && isUsernameValid && isPasswordValid && isEmailValid) {
+            signUpFieldset.innerHTML = "";
+
+            const createdAccMessage = document.createElement("h2");
+            createdAccMessage.textContent = "Account created";
+
+            const userNameElement = document.createElement("p");
+            userNameElement.textContent = "Username: " + username.value;
+
+            const passwordElement = document.createElement("p");
+            passwordElement.textContent = "Password: " +password.value;
+
+            appendChildren(signUpFieldset, [
+                closeFormBtn,
+                createdAccMessage,
+                userNameElement,
+                passwordElement
+            ]);
+        }
+        else {
+            e.preventDefault();
+        }
+    }
+
+    submitBtn.addEventListener("click", formValidation);
 
     const formErrorTextContainer = document.createElement("div");
 
@@ -85,6 +146,10 @@ const createSignUpForm = () => {
         fName,
         lNameLabel,
         lName,
+        usernameLabel,
+        username,
+        passwordLabel,
+        password,
         emailLabel,
         email,
         formBtnContainer,
